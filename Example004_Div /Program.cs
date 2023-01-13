ebb7a7aaa76da03161d95c2a27ac0e6dacd66730
1234 <a1234@MacBook-Pro-1234.local>
2023-01-13 18:58:49 +0300

    add random

--- Example004_Div /Program.cs
+++ Example004_Div /Program.cs
@@ -1,4 +1,5 @@
-﻿double numberA = 12;
-double numberB = 5;
-System.Console.WriteLine(numberA / numberB);
+﻿double numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9 
+double numberB = new Random().Next(1, 10); 
+int result = numberA + numberB;
+Console.WriteLine(result);
  
