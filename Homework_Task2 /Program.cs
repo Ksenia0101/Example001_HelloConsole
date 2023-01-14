5c06bb5ef49bee6b1ad7173100cd1b15a75bcba6
1234 <a1234@MacBook-Pro-1234.local>
2023-01-14 17:34:14 +0300

    DID THE 1ST HOMWORK TASK

---
+++ Homework_Task2 /Program.cs
@@ -0,0 +1,12 @@
+﻿int a, b, max;
+string str;
+Console.WriteLine("Введите число a");
+str = Console.ReadLine();
+a = Convert.ToInt32(str);
+Console.WriteLine("Введите число b");
+str = Console.ReadLine();
+b = Convert.ToInt32(str);
+if (a > b)
+    Console.WriteLine("a - max, b - min");
+else 
+    Console.WriteLine("b - max, a - min");
