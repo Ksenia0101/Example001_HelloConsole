d3abec3c4088a04326bb3699165f5110fdade3a8
1234 <a1234@MacBook-Pro-1234.local>
2023-01-14 16:10:51 +0300

    did the 1st group task

---
+++ Example007_Magic /Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.SetCursorPosition(10, 4);
+Console.WriteLine("+");
+
+
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 40;
+int yc = 30;
+
+Consol.SetCursorPosition(xa, ya);
+ConcurrentExclusiveSchedulerPair.
---
+++ TaskInGroup_1/Program.cs
@@ -0,0 +1,12 @@
+﻿int a; 
+int b;
+string str;
+str = Console.ReadLine();
+a = Convert.ToInt32(str);
+str = Console.ReadLine();
+b = Convert.ToInt32(str);
+if (b * b == a)
+  Console.WriteLine("да");
+else
+  Console.WriteLine("нет");
+
