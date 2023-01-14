f2ef8475393c7ade0ef7ea82be757888b8c869ed
1234 <a1234@MacBook-Pro-1234.local>
2023-01-14 19:25:33 +0300

    did the 8th homework task

---
+++ Homework_Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿int N ;
+int a = 1;
+
+Console.Write("Введите N: ");
+string str = Console.ReadLine();
+N = Convert.ToInt32(str);
+
+while (a <= N)
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine(a);
+    }
+
+    a = a + 1;
+}
