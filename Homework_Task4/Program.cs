db5c50741533fde246361f2f09779c0cda9cc72d
1234 <a1234@MacBook-Pro-1234.local>
2023-01-14 18:06:37 +0300

    did the 4d homework task

---
+++ Homework_Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿int a, b, c, max;
+string str;
+Console.WriteLine("Введите число a");
+str = Console.ReadLine();
+a = Convert.ToInt32(str);
+Console.WriteLine("Введите число b");
+str = Console.ReadLine();
+b = Convert.ToInt32(str);
+Console.WriteLine("Введите число c");
+str = Console.ReadLine();
+c = Convert.ToInt32(str);
+if (a > b)
+    max = a;
+else 
+    max = b;
+
+if (max > c)
+    Console.WriteLine(max);
+else 
+    Console.WriteLine(c);
