c1a66be41fc8a6d651856e0ebf9bc214185b8de1
1234 <a1234@MacBook-Pro-1234.local>
2023-01-14 18:35:25 +0300

    did the 6th homework task

---
+++ Homework_Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿int a;
+string str;
+Console.WriteLine("Введите число a");
+str = Console.ReadLine();
+a = Convert.ToInt32(str);
+if (a % 2 == 0)
+    Console.WriteLine("четное");
+else
+    Console.WriteLine("нечетное");
