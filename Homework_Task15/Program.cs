0815afe3162b45b4540d00fccc393b63c8723d27
1234 <a1234@MacBook-Pro-1234.local>
2023-01-18 13:18:54 +0300

    did the 15th task

---
+++ Homework_Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿String str; 
+int a;
+Console.WriteLine("Введите число");
+str = Console.ReadLine();
+if (int.TryParse(str, out a) == true)
+{
+    if (a == 6 || a == 7)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+else
+{
+    Console.WriteLine("Это не число");
+}
