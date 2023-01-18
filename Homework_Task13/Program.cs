a2f88e9e41e2186aa1f4bb7df162e77b19a15aa5
1234 <a1234@MacBook-Pro-1234.local>
2023-01-18 13:02:04 +0300

    did the 13th homework task

---
+++ Homework_Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿String str;
+int num;
+Console.WriteLine("Введите число:");
+str = Console.ReadLine();
+if (int.TryParse(str, out num) == true)
+{
+    if (str.Length >= 3)
+        {
+        Console.WriteLine(str[2]);
+        }
+        else
+        {
+        Console.WriteLine("Третьей цифры нет");
+        }
+}
+else
+{
+    Console.WriteLine("Это не число");
+}
+
