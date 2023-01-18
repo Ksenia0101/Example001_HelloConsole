6259853b05751cdfb891d9593a8f6d8c3ed38cac
1234 <a1234@MacBook-Pro-1234.local>
2023-01-18 12:37:45 +0300

    did the 10th homework task

---
+++ Demonstration1Lesson2 /Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int a, int b);  
+
+{
+    if (a > b) return a; 
+    else return b;
+}
+
+Random rnd = new Random();
+
+int = rnd.Next(10,100); // 45
+
+int firstNumber = a / 10;
+int secondNumber = a % 10;
+
+int max Max(firstNumber, secondNumber);
+
+System.Consol.WritLine("Max ={max}");
---
+++ Homework_Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿int num;
+string str;
+Console.WriteLine("Введите трехзначное число");
+str = Console.ReadLine();
+//Здесь проверяем строку является ли она числом
+if (int.TryParse(str, out num) == true)
+{
+Console.WriteLine(str[1]);
+}
+else
+{
+    Console.WriteLine("Это не число");
+}
