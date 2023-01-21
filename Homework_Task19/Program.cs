75a9235a2e3f64a932a2058437d0176bf78e1857
1234 <a1234@MacBook-Pro-1234.local>
2023-01-21 23:03:03 +0300

    did the 19th task

--- Homework_Task19/Program.cs
+++ Homework_Task19/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//12321
+//01234 
+String str;
+int result;
+Console.Write("Введите число: ");
+str = Console.ReadLine();
+if (int.TryParse(str,out result) == true)
+{
+  int lastIndex = str.Length - 1;
+  bool isPalindrom = true;
+  for (int i = 0; i < str.Length / 2; i++)
+  {
+    if (str[i] != str[lastIndex - i])
+    {
+        isPalindrom = false;
+        break;
+    }   
+  }
+  if (isPalindrom)
+  {
+    Console.WriteLine("Введённое число является палиндромом");
+  }
+  else
+  {
+    Console.WriteLine("Введённое число НЕ является палиндромом");
+  }
+}
+else 
+{
+    Console.WriteLine("Вы ввели не число!");
+}
--- Homework_Task21/Program.cs
+++ Homework_Task21/Program.cs
@@ -26,10 +26,14 @@ bool ReadCoords(out double[] Point)
 
 double[] A = new double[3];
 double[] B = new double[3];
-double distance;
+double distance = 0;
 string str;
 if (ReadCoords(out A) && ReadCoords(out B))
 {
-    distance = Math.Sqrt(Math.Pow(B[0] - A[0], 2) + Math.Pow(B[1] - A[1], 2) + Math.Pow(B[2] - A[2], 2));
+    for (int i = 0; i < 3; i++)
+    {
+        distance = distance + Math.Pow(B[i] - A[i], 2);
+    }
+    distance = Math.Sqrt(distance);
     Console.WriteLine("Расстояние между точками А и B: " + distance);
 }
