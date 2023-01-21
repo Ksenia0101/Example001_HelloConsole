e0002e1b7b02c7862fa40336a8665290a556718f
1234 <a1234@MacBook-Pro-1234.local>
2023-01-21 23:25:42 +0300

    did the 23th task

--- Homework_Task23/Program.cs
+++ Homework_Task23/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int N;
+int i = 1;
+String str;
+Console.WriteLine("Введите число N");
+str = Console.ReadLine();
+N = Convert.ToInt32(str);
+while (i < N)
+{
+    Console.WriteLine(i + "^3 = " + Math.Pow(i, 3)); //3^3 = 27
+    i++;
+}
+
