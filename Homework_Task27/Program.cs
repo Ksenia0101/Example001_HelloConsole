7b3bec543769986bc699d06ea1239a8a3b9041bb
1234 <a1234@MacBook-Pro-1234.local>
2023-01-26 18:14:33 +0300

    did the 25th task

---
+++ Homework_Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while(A > 0)
+{
+    sum += A % 10;
+    A = A / 10;
+}
+Console.WriteLine("Сумма цифр: " + sum);
