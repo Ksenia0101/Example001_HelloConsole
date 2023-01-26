995b39ff4c55825373f49d075f3aae36bcb47693
1234 <a1234@MacBook-Pro-1234.local>
2023-01-26 20:00:59 +0300

    did the 29th task

--- Homework_Task29/Program.cs
+++ Homework_Task29/Program.cs
@@ -1,1 +1,22 @@
 ﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] CreateArray(int Size)
+{
+    int[] result = new int[Size];
+    for (int i = 0; i < Size; i++)
+    {
+        result[i] = i;
+    }
+    return result;
+}
+
+void PrintArray(int[] InArray)
+{
+    for (int i = 0; i < InArray.Length; i++)
+    {
+        Console.Write($"{InArray[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] A = CreateArray(8);
+PrintArray(A);
