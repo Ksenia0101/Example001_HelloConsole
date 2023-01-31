3d89e6fe580a6291ea5f2d8f9990a241bc4caadb
1234 <a1234@MacBook-Pro-1234.local>
2023-01-31 15:55:35 +0300

    did the 36th task

--- Homework_Task36/Program.cs
+++ Homework_Task36/Program.cs
@@ -1,1 +1,34 @@
 ﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] Massive(int size)
+{
+ Random random = new Random();
+ int[] result = new int[size];
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = random.Next(1, 100);
+    }
+    return result;
+}
+
+void Print(int[] Arr)
+{
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        Console.Write(Arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] A = Massive(10); 
+Print(A);
+int sum = 0;
+for (int i = 0; i < A.Length; i++)
+{
+  if (i % 2 != 0)   
+  {
+    sum = sum + A[i];
+  }
+}
+Console.WriteLine(sum);
