5958b9f1050e5d4d887f8e105f2c1eed30d19871
1234 <a1234@MacBook-Pro-1234.local>
2023-01-31 21:15:38 +0300

    did the 38th task

--- Homework_Task38/Program.cs
+++ Homework_Task38/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+double[] Massive(int size)
+{
+    Random random = new Random();
+    double[] result = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = random.NextDouble() * 10;
+    }
+    return result;
+}
+void PrintArray(double[] Arr)
+{
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        Console.Write(Arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+int size = 10;
+double min;
+double max;
+int index = 0;
+
+double[] A = Massive(size);
+min = A[0];
+max = A[0];
+PrintArray(A);
+
+
+while (index < size)
+{
+    if (A[index] > max)
+    {
+        max = A[index];
+    }
+
+    if (A[index] < min)
+    {
+        min = A[index];
+    }
+    index++;
+}
+Console.WriteLine("Разница между максимальным и минимальным элементами массива: " + (max - min));
+
+
+
+
+
