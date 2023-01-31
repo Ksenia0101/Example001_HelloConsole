fc28618c9be5d9d2791e93e1b4dc5f63a0335773
1234 <a1234@MacBook-Pro-1234.local>
2023-01-31 14:37:30 +0300

    did th 34th task

---
+++ Homework_Task34/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] FillArray(int size, int min, int max)
+{
+   Random random = new Random(); 
+   int[] arr = new int[size];
+   for (int i = 0; i < size; i++)
+   {
+    arr[i] = random.Next(min, max);
+   }
+   return arr;
+}
+void Print(int[] MASSIVE )
+{
+    for (int i = 0; i < MASSIVE.Length; i++)
+    {
+        Console.Write(MASSIVE[i]+" ");
+    }
+    Console.WriteLine();
+}
+
+int[] a = FillArray(10, 100, 1000);
+Print(a);
+ 
