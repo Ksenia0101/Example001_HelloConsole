b6fc933b553b25ef765f004391a123f384480567
1234 <a1234@MacBook-Pro-1234.local>
2023-02-02 17:56:30 +0300

    did the 41th task

---
+++ Homework_Task41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int M = 10;
+int i = 0;
+int counter = 0;
+while (i < 10)
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) 
+    {
+      counter++; 
+        
+    }
+
+    i++;
+}
+Console.WriteLine("Чисел больше нуля: " + counter );
