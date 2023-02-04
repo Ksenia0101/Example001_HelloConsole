db430c202aae77e00fbb84112570b60413dfb5f1
1234 <a1234@MacBook-Pro-1234.local>
2023-02-04 20:04:44 +0300

    did the 47th task

---
+++ Homework_Task47/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+int m = 3;
+int n = 4;
+double[,] Matrix()
+{
+    Random random = new Random();
+    double[,] result = new double[m,n];
+    for(int i = 0; i < m; i++)
+      for(int j = 0; j < n; j++)
+        {
+        result[i,j] = random.NextDouble()* 10;
+        }
+
+    return result;
+}
+
+void PrintMatrix(double[,] Matrix)
+{
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            Console.Write($"{Matrix[i,j].ToString("0.##")} ");
+        }
+    Console.WriteLine();
+    }   
+      
+}
+PrintMatrix(Matrix());
